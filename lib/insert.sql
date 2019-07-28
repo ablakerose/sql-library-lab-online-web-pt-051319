b9a943794833a24a00dc31b46097854e7b5545f8
Blake Rose <bdecktor@gmail.com>
2019-07-28 17:08:34 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO series
+(title, author_id, subgenre_id)
+VALUES ("A Song of Ice and Fire", 1, 1);
+
+INSERT INTO series
+(title, author_id, subgenre_id)
+VALUES ("The Hunger Games", 2, 2);
+
+INSERT INTO subgenres (name) VALUES ("medieval"), ("space opera");
+
+INSERT INTO authors (name) VALUES ("George R. R. Martin"), ("Suzanne Collins");
+
+INSERT INTO books (title, year, series_id) VALUES ("Game of Thrones", 1996, 1), ("A Clash of Kings", 1998, 1), ("A Storm of Swords", 2000, 1), ("First Book", 2002, 2), ("Second Book", 2003, 2), ("Third Book", 2005, 2);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
